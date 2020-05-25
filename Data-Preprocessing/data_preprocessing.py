b60328fb30a8a2f1e40d2878f4edce81715738dd
Jeevan-M <csejeevanm@gmail.com>
2020-05-25 18:19:15 +0530

    filling the missing data

--- Data-Preprocessing/data_preprocessing.py
+++ Data-Preprocessing/data_preprocessing.py
@@ -8,15 +8,14 @@ Created on Sat May 23 19:52:31 2020
 import numpy as np
 import matplotlib.pyplot as plt 
 import pandas as pd 
-from sklearn.preprocessing import Imputer
+from sklearn.impute import SimpleImputer
 # importing the dataset
 dataset = pd.read_csv('Data.csv')
 x = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,3].values
 
-
 # Tacking care of missing data
 
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean',axis = 0)
+imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
 imputer = imputer.fit(x[:,1:3])
 x[:,1:3] = imputer.transform(x[:,1:3])
